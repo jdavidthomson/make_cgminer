0eaa2d02cf47ab8f12b82ac2971926277b9ca00c
jdavidthomson <33036919+jdavidthomson@users.noreply.github.com>
2017-10-24 16:24:09 -0400

    forgot the apt update
    for use when launching a new instance on AWS

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sudo apt update
 sudo apt install g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
 
 wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run
