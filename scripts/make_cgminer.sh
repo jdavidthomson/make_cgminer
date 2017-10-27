1eeb7b6b37588ead07d48d8b1fd755c85f4ae47c
David Thomson <jdavidthomson@gmail.com>
2017-10-27 10:18:24 -0500

    from two down to one script

--- scripts/make_cgminer.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/jdavidthomson/cgminer.git && cd cgminer && git checkout v3.7.2-knc
-./autogen.sh --enable-opencl
-make
--- scripts/script.sh
+++ scripts/script.sh
@@ -9,3 +9,7 @@ sudo ./cuda_9.0.176_384.81_linux-run --override
 sudo apt install pkg-config libtool automake
 sudo apt install libcurl4-openssl-dev
 sudo apt install opencl-headers
+
+git clone https://github.com/jdavidthomson/cgminer.git && cd cgminer && git checkout v3.7.2-knc
+./autogen.sh --enable-opencl
+make
